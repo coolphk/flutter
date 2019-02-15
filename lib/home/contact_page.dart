5b81829975048a823f1de01bb0b80ec2bf657566
coolphk <25145168@qq.com>
2019-02-15 11:55:52 +0800

    contact

--- lib/home/contact_page.dart
+++ lib/home/contact_page.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import '../modal/contact.dart';
 
 class _ContactItem extends StatelessWidget {
-  final Contact contact;
+  final String avatar;
+  final String title;
+  final String groupTitle;
 
-  const _ContactItem({Key key, this.contact}) : super(key: key);
+  _ContactItem({@required this.avatar, @required this.title, this.groupTitle});
 
   @override
   Widget build(BuildContext context) {
@@ -22,10 +24,14 @@ class ContactsPage extends StatefulWidget {
 }
 
 class _ContactsPageState extends State<ContactsPage> {
+  final List<Contact> _contacts = ContactPageData.mock().contacts;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.deepPurple,
-    );
+    return ListView.builder(itemBuilder: (BuildContext context, int index) {
+      return _ContactItem(
+        avatar: _contacts[index].avatar,
+      );
+    });
   }
 }
