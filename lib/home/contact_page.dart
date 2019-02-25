ab6f91a8be6fb8e9e73962f9a81c2a53bd2e55c9
coolphk <25145168@qq.com>
2019-02-25 16:31:23 +0800

    cotact.groupTitle

--- lib/home/contact_page.dart
+++ lib/home/contact_page.dart
@@ -17,32 +17,52 @@ class _ContactItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: EdgeInsets.symmetric(horizontal: 13.0),
-        child: Container(
-          decoration: BoxDecoration(
-              border: Border(
-                  bottom: BorderSide(
-                      color: Color(AppColors.DividerColor),
-                      width: Constants.DividerWidth))),
-          padding: EdgeInsets.symmetric(vertical: 10.0),
-          child: Row(
-            children: <Widget>[
-              Container(
-                child: Tools.getAvatar(avatar, Constants.ContactAvatarSize),
-              ),
-              SizedBox(
-                width: 10.0,
-              ),
-              Container(
-                child: Text(
-                  title,
-                  style: AppStyles.TitleStyle,
-                ),
-              )
-            ],
+    //列表项主体部分
+    Widget _button = Container(
+      decoration: BoxDecoration(
+          border: Border(
+              bottom: BorderSide(
+                  color: Color(AppColors.DividerColor),
+                  width: Constants.DividerWidth))),
+      padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 13.0),
+      child: Row(
+        children: <Widget>[
+          Container(
+            child: Tools.getAvatar(avatar, Constants.ContactAvatarSize),
           ),
-        ));
+          SizedBox(
+            width: 10.0,
+          ),
+          Container(
+            child: Text(
+              title,
+              style: AppStyles.TitleStyle,
+            ),
+          )
+        ],
+      ),
+    );
+    //分组标签
+    Widget _itemBody;
+
+    if (this.groupTitle != null) {
+      _itemBody = Column(
+        children: <Widget>[
+          Container(
+            color: Color(AppColors.ContactGroupTitleBg),
+            alignment: Alignment.centerLeft,
+            child: Container(
+                padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 13.0),
+                child: Text(this.groupTitle,
+                    style: AppStyles.GroupTitleItemTextStyle)),
+          ),
+          _button
+        ],
+      );
+    } else {
+      _itemBody = _button;
+    }
+    return Container(child: _itemBody);
   }
 }
 
@@ -58,7 +78,7 @@ class _ContactsPageState extends State<ContactsPage> {
     _ContactItem(
       avatar: 'assets/images/ic_new_friend.png',
       title: '新的朋友',
-      onPressed: (){
+      onPressed: () {
         print('新的朋友');
       },
     ),
@@ -86,7 +106,9 @@ class _ContactsPageState extends State<ContactsPage> {
     _contacts
       ..addAll(data.contacts)
       ..addAll(data.contacts)
-      ..addAll(data.contacts);
+      ..addAll(data.contacts)
+      ..sort((a, b) => a.name.compareTo(b.name));
+    print(_contacts);
   }
 
   @override
@@ -96,10 +118,11 @@ class _ContactsPageState extends State<ContactsPage> {
         if (index < _functionButtons.length) {
           return _functionButtons[index];
         } else {
-          var contact = _contacts[index];
+          var contact = _contacts[index - _functionButtons.length];
           return _ContactItem(
             avatar: contact.avatar,
             title: contact.name,
+            groupTitle: contact.nameIndex,
           );
         }
       },
